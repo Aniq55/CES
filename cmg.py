8c9d83805e232504420817bf38a7fd8e3551038c
Aniq55 <aniqrah@gmail.com>
2024-10-29 15:35:30 +0000

    Algo 1 and 2

---
+++ cmg.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pickle
+
+
+# Parameters
+n = 10
+p_0 = 0.4
+theta_0 = 3e-5
+mu_0 = 5.5
+sigma_0 = 1
+sigma_1 = 1.5
+sigma_2 = 0.7
+
+
+# step 1
+A = np.zeros((n,n))
+for i in range(n):
+    for j in range(n):
+        A[i][j] = np.random.binomial(1, p_0)
+
+# step 2
+for i in range(n):
+    A[i][i] = 1
+
+
+# step 4
+Lambda = np.zeros((n,1))
+for i in range(n):
+    Lambda[i] = np.random.lognormal(mu_0, sigma_0**2)
+
+
+# step 5
+Eta = np.zeros((n,1))
+for i in range(n):
+    Eta[i] = np.random.normal(0, sigma_1**2)
+
+
+# steps 6-10
+Alpha = np.zeros((n,n))
+Beta = np.zeros((n,n))
+Gamma = np.zeros((n,n))
+for i in range(n):
+    for j in range(n):
+        if A[i][j] == 1:
+            Beta[i][j] = np.random.exponential(theta_0)
+            Alpha[i][j] = Beta[i][j]/Lambda[j]
+            Gamma[i][j] = np.random.normal(0, sigma_2**2)
+
+
+# step 11
+Theta = {
+    'n': n,
+    'Lambda': Lambda,
+    'Eta': Eta,
+    'Alpha': Alpha,
+    'Beta': Beta,
+    'Gamma': Gamma
+}
+
+with open('/home/chri6578/Documents/CES/params/Theta_1.pickle', 'wb') as file:
+    pickle.dump(Theta, file)
+
+
---
+++ esg.py
@@ -0,0 +1,64 @@
+import numpy as np 
+import pickle
+
+
+def sigmoid(x):
+    return 1.0/(1 + np.exp(-x))
+
+
+param_name = "Theta_1"
+
+with open(f'/home/chri6578/Documents/CES/params/{param_name}.pickle', 'rb') as file:
+    Theta = pickle.load(file)
+    
+n = Theta['n']
+Lambda = Theta['Lambda']
+Eta = Theta['Eta']
+Alpha = Theta['Alpha']
+Beta = Theta['Beta']
+Gamma = Theta['Gamma']
+T = 1e3
+
+
+# step 2
+PHI = {}
+
+for i in range(n):
+    PHI[i] = []
+    t = 0
+    t_prev = 0
+    while t_prev < T:
+        t = np.random.exponential(Lambda[i])[0]
+        PHI[i].append(t + t_prev)
+        t_prev = t + t_prev
+
+
+# step 3
+tuple_list = [(i, t) for i in range(n) for t in PHI[i]]
+T_list = sorted(tuple_list, key=lambda x: x[1])
+
+
+# steps 4-11
+E_all = {}
+for i in range(n):
+    E_all[i] = []
+t_last = np.zeros((n,))
+
+for (e,t) in T_list:
+    # update x_i^t
+    s = np.sum( Gamma[e]*np.power(t- t_last, Alpha[e]-1)*np.exp(-(t-t_last)*Beta[e])*(t_last != 0) ) + Eta[e]
+    p = sigmoid(s)
+    # print(s, p)
+    trial = np.random.binomial(1,p)
+    if trial:
+        E_all[e].append(t)
+        t_last[e] = t
+
+with open('/home/chri6578/Documents/CES/sequence/ES_Theta_1.pickle', 'wb') as file:
+    pickle.dump(E_all, file)
+
+
+
+
+
+
